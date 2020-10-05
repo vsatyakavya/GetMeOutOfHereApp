0c9a2863b45e920f7722417ba998f656396fcaf8
Ted Peters <44210809+Drop-G@users.noreply.github.com>
2020-10-05 17:21:03 -0500

    added mapbox API and Map outline

---
+++ logic.js
@@ -0,0 +1,6 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZHJvcC1nIiwiYSI6ImNrZnd3OTd6azFvMWkydG10aGN2Z2Q2MnUifQ.EOjRwzS_WYEK5wOfXc32sQ';
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
+  });
+
