8032fac94531e5c193772c7eec310c6cc735ec17
Eliza Kennelly <elizakennelly@gmail.com>
2020-10-06 14:42:45 -0500

    console log map info

--- logic.js
+++ logic.js
@@ -1,6 +1,18 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiZHJvcC1nIiwiYSI6ImNrZnd3OTd6azFvMWkydG10aGN2Z2Q2MnUifQ.EOjRwzS_WYEK5wOfXc32sQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoiZWxpemFqYW5lMTMxMiIsImEiOiJja2Z3eDI2MDcxbnk3Mnh0c2VrNmk5eXRvIn0.NNut521VI5QdeK_RuDbCiA';
   var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11'
+    style: 'mapbox://styles/mapbox/outdoors-v11',
+    zoom: 4
   });
-
+  console.log(map);
+  map.addControl(
+    new mapboxgl.GeolocateControl({
+    positionOptions: {
+    enableHighAccuracy: true
+    },
+    trackUserLocation: true
+    })
+    );
+// Want map to grab a list of 10 features from the user's location parameters (userdropdown menu mileage)
+// When one of those features is clicked, new page opens with that feature's info
+// Maybe also include a dropdown menu so they can choose how many features they want to see (10, 50, etc.) ?
