39de5afea2e642ec81d9975f8200152c2bc37100
satya kavya <satyakavya24@gmail.com>
2020-10-06 11:33:46 -0500

    add branch

--- logic.js
+++ logic.js
@@ -4,3 +4,24 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZHJvcC1nIiwiYSI6ImNrZnd3OTd6azFvMWkydG10aGN2Z
     style: 'mapbox://styles/mapbox/streets-v11'
   });
 
+  $(document).ready(function(){
+$('.dropdown-trigger').dropdown();
+  });
+//   var queryURL="https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic2F0eWFrYXZ5YSIsImEiOiJja2Z3dzlqMHQxbms4MnFtdHU2anRzdXJ1In0.6_LfOlTLPu9B8frAVcNLqA"
+
+// $.ajax({
+//     url:queryURL,
+//     method:"GET"
+// }).then(function(response){
+//     console.log(response);
+// })
+
+// var queryURL="https://api.mapbox.com/geocoding/v5/mapbox.places/starbucks.json?bbox=-77.083056,38.908611,-76.997778,38.959167&access_token=pk.eyJ1Ijoic2F0eWFrYXZ5YSIsImEiOiJja2Z3dzlqMHQxbms4MnFtdHU2anRzdXJ1In0.6_LfOlTLPu9B8frAVcNLqA"
+
+// $.ajax({
+//     url:queryURL,
+//     method:"GET"
+// }).then(function(response){
+//     console.log(response);
+// })
+
