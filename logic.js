c562d4dcb25fb615ece0812f4e29f7ecbeae62a5
satya kavya <satyakavya24@gmail.com>
2020-10-07 01:37:25 -0500

    add main code

--- logic.js
+++ logic.js
@@ -1,9 +1,12 @@
-
-
-$(document).ready(function(){
-$('.dropdown-trigger').dropdown();
-
-$('.carousel').carousel();
+mapboxgl.accessToken = 'pk.eyJ1IjoiZHJvcC1nIiwiYSI6ImNrZnd3OTd6azFvMWkydG10aGN2Z2Q2MnUifQ.EOjRwzS_WYEK5wOfXc32sQ';
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
   });
 
+  $(document).ready(function(){
+$('.dropdown-trigger').dropdown();
+$('select').formSelect();
+$('.carousel').carousel();
+  });
 
