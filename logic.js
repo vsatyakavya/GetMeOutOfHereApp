a0eb2ddfd0fe58173ee033e514e297d21b4063f1
satya kavya <satyakavya24@gmail.com>
2020-10-07 16:21:46 -0500

    getting objects from mapbox

--- logic.js
+++ logic.js
@@ -1,73 +1,107 @@
-$(document).ready(function(){
-  $('.dropdown-trigger').dropdown();
-});
-$(document).ready(function(){
+
+$(document).ready(function () {
   $('.dropdown-trigger').dropdown();
   $('select').formSelect();
   $('.carousel').carousel();
-    
 
-// mapboxgl.accessToken = 'pk.eyJ1IjoiZHJvcC1nIiwiYSI6ImNrZnd3OTd6azFvMWkydG10aGN2Z2Q2MnUifQ.EOjRwzS_WYEK5wOfXc32sQ';
-//   var map = new mapboxgl.Map({
-//     container: 'map',
-//     style: 'mapbox://styles/mapbox/streets-v11'
-//   });
 
-  if (navigator.geolocation)
-  {
-      navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+  // mapboxgl.accessToken = 'pk.eyJ1IjoiZHJvcC1nIiwiYSI6ImNrZnd3OTd6azFvMWkydG10aGN2Z2Q2MnUifQ.EOjRwzS_WYEK5wOfXc32sQ';
+  //   var map = new mapboxgl.Map({
+  //     container: 'map',
+  //     style: 'mapbox://styles/mapbox/streets-v11'
+  //   });
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
   }
-  else 
-  {
-      alert('It seems like Geolocation, which is required for this page, is not enabled in your browser.');
-  }       
+  else {
+    alert('It seems like Geolocation, which is required for this page, is not enabled in your browser.');
+  }
+
+
+  function errorFunction(position) {
+    alert('Error!');
+  }
+
+  function successFunction(position) {
+    var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZHJvcC1nIiwiYSI6ImNrZnd3OTd6azFvMWkydG10aGN2Z2Q2MnUifQ.EOjRwzS_WYEK5wOfXc32sQ';
+    var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [long, lat],
+      zoom: 9
+    });
+
+
+
+    var queryURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/theme%20park.json?proximity=" + long + "," + lat + "&access_token=pk.eyJ1Ijoic2F0eWFrYXZ5YSIsImEiOiJja2Z3dzlqMHQxbms4MnFtdHU2anRzdXJ1In0.6_LfOlTLPu9B8frAVcNLqA"
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+    })
+
+    var queryURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/lake.json?proximity=" + long + "," + lat + "&autocomplete=false&access_token=pk.eyJ1Ijoic2F0eWFrYXZ5YSIsImEiOiJja2Z3dzlqMHQxbms4MnFtdHU2anRzdXJ1In0.6_LfOlTLPu9B8frAVcNLqA"
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+    })
+    var queryURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/dog%20park.json?proximity=" + long + "," + lat + "&autocomplete=false&types=poi&access_token=pk.eyJ1Ijoic2F0eWFrYXZ5YSIsImEiOiJja2Z3dzlqMHQxbms4MnFtdHU2anRzdXJ1In0.6_LfOlTLPu9B8frAVcNLqA"
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+    })
+
+    var queryURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/beach.json?proximity=" + long + "," + lat + "&autocomplete=false&access_token=pk.eyJ1Ijoic2F0eWFrYXZ5YSIsImEiOiJja2Z3dzlqMHQxbms4MnFtdHU2anRzdXJ1In0.6_LfOlTLPu9B8frAVcNLqA"
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+    })
+
+    var queryURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/campground.json?proximity=" + long + "," + lat + "&autocomplete=false&access_token=pk.eyJ1Ijoic2F0eWFrYXZ5YSIsImEiOiJja2Z3dzlqMHQxbms4MnFtdHU2anRzdXJ1In0.6_LfOlTLPu9B8frAVcNLqA"
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+    })
+
+    var queryURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/zoo.json?proximity=" + long + "," + lat + "&autocomplete=false&access_token=pk.eyJ1Ijoic2F0eWFrYXZ5YSIsImEiOiJja2Z3dzlqMHQxbms4MnFtdHU2anRzdXJ1In0.6_LfOlTLPu9B8frAVcNLqA"
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+    })
+    var queryURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/pool.json?proximity=" + long + "," + lat + "&autocomplete=false&access_token=pk.eyJ1Ijoic2F0eWFrYXZ5YSIsImEiOiJja2Z3dzlqMHQxbms4MnFtdHU2anRzdXJ1In0.6_LfOlTLPu9B8frAVcNLqA"
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+    })
+
+  }
 });
 
-function successFunction(position) 
-{
-  var lat = position.coords.latitude;
-  var long = position.coords.longitude;
-  alert('Your latitude is :'+lat+' and longitude is '+long);
-}
-
-function errorFunction(position) 
-{
-  alert('Error!');
-}
-var queryURL="https://api.mapbox.com/geocoding/v5/mapbox.places/30.473656599999998,-97.65708339999999.json?access_token=pk.eyJ1Ijoic2F0eWFrYXZ5YSIsImEiOiJja2Z3dzlqMHQxbms4MnFtdHU2anRzdXJ1In0.6_LfOlTLPu9B8frAVcNLqA"
-$.ajax({
-  url:queryURL,
-  method:"GET"
-}).then(function(response){
-  console.log(response);
-})
-
-  var queryURL="https://api.mapbox.com/geocoding/v5/mapbox.places/roundrock.json?access_token=pk.eyJ1Ijoic2F0eWFrYXZ5YSIsImEiOiJja2Z3dzlqMHQxbms4MnFtdHU2anRzdXJ1In0.6_LfOlTLPu9B8frAVcNLqA"
-
-$.ajax({
-    url:queryURL,
-    method:"GET"
-}).then(function(response){
-    console.log(response);
-})
-
-var queryURL="https://api.mapbox.com/geocoding/v5/mapbox.places/theme%20park.json?bbox=-98.0261770004211,30.0678640288457,-97.5416001207618,30.5194859159522&access_token=pk.eyJ1Ijoic2F0eWFrYXZ5YSIsImEiOiJja2Z3dzlqMHQxbms4MnFtdHU2anRzdXJ1In0.6_LfOlTLPu9B8frAVcNLqA"
-
-$.ajax({
-    url:queryURL,
-    method:"GET"
-}).then(function(response){
-    console.log(response);
-})
-
-var queryURL="https://api.mapbox.com/geocoding/v5/mapbox.places/chester.json?proximity=-74.70850,40.78375&access_token=pk.eyJ1Ijoic2F0eWFrYXZ5YSIsImEiOiJja2Z3dzlqMHQxbms4MnFtdHU2anRzdXJ1In0.6_LfOlTLPu9B8frAVcNLqA"
-$.ajax({
-    url:queryURL,
-    method:"GET"
-}).then(function(response){
-    console.log(response);
-})
 
 
 
- 
