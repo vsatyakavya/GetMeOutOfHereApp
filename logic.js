40f64327dbf59c952646dc2b0a54088159639679
satya kavya <satyakavya24@gmail.com>
2020-10-07 01:15:45 -0500

    remove unwanted code

--- logic.js
+++ logic.js
@@ -1,29 +1,5 @@
-// mapboxgl.accessToken = 'pk.eyJ1IjoiZHJvcC1nIiwiYSI6ImNrZnd3OTd6azFvMWkydG10aGN2Z2Q2MnUifQ.EOjRwzS_WYEK5wOfXc32sQ';
-//   var map = new mapboxgl.Map({
-//     container: 'map',
-//     style: 'mapbox://styles/mapbox/streets-v11'
-//   });
-
   $(document).ready(function(){
 $('.dropdown-trigger').dropdown();
 $('select').formSelect();
 $('.carousel').carousel();
   });
-//   var queryURL="https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic2F0eWFrYXZ5YSIsImEiOiJja2Z3dzlqMHQxbms4MnFtdHU2anRzdXJ1In0.6_LfOlTLPu9B8frAVcNLqA"
-
-// $.ajax({
-//     url:queryURL,
-//     method:"GET"
-// }).then(function(response){
-//     console.log(response);
-// })
-
-// var queryURL="https://api.mapbox.com/geocoding/v5/mapbox.places/starbucks.json?bbox=-77.083056,38.908611,-76.997778,38.959167&access_token=pk.eyJ1Ijoic2F0eWFrYXZ5YSIsImEiOiJja2Z3dzlqMHQxbms4MnFtdHU2anRzdXJ1In0.6_LfOlTLPu9B8frAVcNLqA"
-
-// $.ajax({
-//     url:queryURL,
-//     method:"GET"
-// }).then(function(response){
-//     console.log(response);
-// })
-
