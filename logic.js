bc278ee82232eb816585ccf3f4b6232af94a7d20
satya kavya <satyakavya24@gmail.com>
2020-10-07 20:53:08 -0500

    add weather conditions of selected place

--- logic.js
+++ logic.js
@@ -59,42 +59,73 @@ $(document).ready(function () {
         url: queryURL,
         method: "GET"
       }).then(function (response) {
+        console.log(response);
         $("#poi-section").empty();
 
         for (var i = 0; i < 5; i++) {
           var placeName= response.features[i].place_name;
+          var longitude = response.features[i].center[0];
+          var latitude =  response.features[i].center[1];
           var name = $("<p>");
           name.addClass("selectedplace");
-          name.attr("place-name", place);
+          name.attr("place-name", place+i);
+          name.attr("longitude", longitude);
+          name.attr("latitude", latitude);
           name.text(placeName);
 
           if (placeName) {
             $("#poi-section").append(name);
           }
-         
-      
           
         }
+
+        $(".selectedplace").on("click", function(event) {
+          var weatherLong = $(this).attr("longitude");
+          var weatherLat = $(this).attr("latitude");
+          var API = "381913a298d3365457e6e5d4fdf83c2c";
+          var queryUrl = "https://api.openweathermap.org/data/2.5/weather" + "?lat=" + weatherLat + "&lon=" + weatherLong + "&appid=" + API + "&units=imperial";
+          $.ajax({
+            url:queryUrl,
+            method:"GET"
+          }).then(function(response){
+            console.log(response);
+            var humidity= response.main.humidity;
+            var temperature =response.main.temp;
+            var date= response.dt;
+            var formattedDate=  Unix_timestamp(date);
+
+            var icon = response.weather[0].icon;
+            
+            var temp = $("<p>");
+            temp.text("Temperature: " + temperature);
+            var humid = $("<p>");
+            humid.text("Humidity: " + humidity);
+            var dat = $("<p>");
+            dat.text("Date: "+ formattedDate);
+            $("#selected-details").append(temp).append(humid).append(dat);
+
+
+          });
+ 
+            // event.preventDefault();
+            // var details =$("<p>");
+            // details.text(weatherLong+" "+weatherLat);
+            // $("#poi-section2").append(details);
+            // getWeatherConditions(weatherLat, weatherLong);
+ 
+          })
+
         
       })
-      // var queryURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/dog%20park.json?proximity=" + long + "," + lat + "&autocomplete=false&types=poi&access_token=pk.eyJ1Ijoic2F0eWFrYXZ5YSIsImEiOiJja2Z3dzlqMHQxbms4MnFtdHU2anRzdXJ1In0.6_LfOlTLPu9B8frAVcNLqA"
-
-      // $.ajax({
-      //   url: queryURL,
-      //   method: "GET"
-      // }).then(function (response) {
-      //   console.log(response);
-      // })
-
-      // var queryURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/beach.json?proximity=" + long + "," + lat + "&autocomplete=false&access_token=pk.eyJ1Ijoic2F0eWFrYXZ5YSIsImEiOiJja2Z3dzlqMHQxbms4MnFtdHU2anRzdXJ1In0.6_LfOlTLPu9B8frAVcNLqA"
-
-      // $.ajax({
-      //   url: queryURL,
-      //   method: "GET"
-      // }).then(function (response) {
-      //   console.log(response);
-      // })
-
+      function Unix_timestamp(t)
+      {
+      var dt = new Date(t * 1000);
+      var date = dt.getDate();
+      var month =  (dt.getMonth() + 1);
+      var year =  dt.getFullYear();
+      return month+ '/' + date+ '/' + year;  
+      }
+      
 
     }
   }
