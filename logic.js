70526a9190e661896114201896150c96811c1bd0
Ted Peters <44210809+Drop-G@users.noreply.github.com>
2020-10-07 19:45:41 -0500

    logic.js updated api key

--- logic.js
+++ logic.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZHJvcC1nIiwiYSI6ImNrZnd3OTd6azFvMWkydG10aGN2Z2Q2MnUifQ.EOjRwzS_WYEK5wOfXc32sQ';
-=======
-mapboxgl.accessToken = 'pk.eyJ1IjoiZWxpemFqYW5lMTMxMiIsImEiOiJja2Z3eDI2MDcxbnk3Mnh0c2VrNmk5eXRvIn0.NNut521VI5QdeK_RuDbCiA';
->>>>>>> EJ/JSfunctions
   var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/outdoors-v11',
@@ -21,22 +17,6 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZWxpemFqYW5lMTMxMiIsImEiOiJja2Z3eDI2MDcxbnk3M
     var queryURL="https://api.mapbox.com/geocoding/v5/mapbox.places/theme%20park.json?bbox=-98.0261770004211,30.0678640288457,-97.5416001207618,30.5194859159522&access_token=pk.eyJ1IjoiZWxpemFqYW5lMTMxMiIsImEiOiJja2Z3eDI2MDcxbnk3Mnh0c2VrNmk5eXRvIn0.NNut521VI5QdeK_RuDbCiA";
     
 
-<<<<<<< HEAD
 
 
 
-=======
-$.ajax({
-    url:queryURL,
-    method:"GET"
-}).then(function(response){
-    console.log(response);
-})
-
-$("submit").on("click", function() {
-  // Insert code that takes user to results page when they click the submit button
-})
-// Want map to grab a list of 5 features from the user's location parameters (userdropdown menu mileage)
-// When one of those features is clicked, new page opens with that feature's info
-// Maybe also include a dropdown menu so they can choose how many features they want to see (10, 50, etc.) ?
->>>>>>> EJ/JSfunctions
