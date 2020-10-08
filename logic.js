d42314f104d38fb164a8a2f4f01acd8f3e929fd2
Ted Peters <44210809+Drop-G@users.noreply.github.com>
2020-10-08 08:20:11 -0500

    marker added

--- logic.js
+++ logic.js
@@ -15,6 +15,10 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZHJvcC1nIiwiYSI6ImNrZnd3OTd6azFvMWkydG10aGN2Z
     })
     );
     var queryURL="https://api.mapbox.com/geocoding/v5/mapbox.places/theme%20park.json?bbox=-98.0261770004211,30.0678640288457,-97.5416001207618,30.5194859159522&access_token=pk.eyJ1IjoiZWxpemFqYW5lMTMxMiIsImEiOiJja2Z3eDI2MDcxbnk3Mnh0c2VrNmk5eXRvIn0.NNut521VI5QdeK_RuDbCiA";
+
+    var marker = new mapboxgl.Marker()
+.setLngLat([lat, long])
+.addTo(map);
     
 
 
